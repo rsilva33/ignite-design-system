203894e8d9da87e6117666acad1717650acf59d9
rsilva33 <ronaldo.silva.jr0105@gmail.com>
2023-11-20 18:09:19 +0000

    publishing storybook

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -26,5 +26,12 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/05-design-system/'
+    }
+
+    return config
+  }
 };
 export default config;
