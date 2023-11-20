09d047b2b2518c2f9a9dcc63da15fa8c5e661c3d
rsilva33 <ronaldo.silva.jr0105@gmail.com>
2023-11-20 16:26:29 +0000

    Controls on components

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -8,9 +8,29 @@ export default {
   tags: ['autodocs'],
   args: {
     children: 'Send',
+    variant: 'primary',
+    size: 'md',
+    disabled: false,
   },
   argTypes: {
     onClick: { action: 'clicked' },
+    variant: {
+      options: ['primary', 'secondary', 'tertiary'],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+    size: {
+      options: ['sm', 'md'],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+    disabled: {
+      control: {
+        type: 'boolean',
+      },
+    },
   },
 } as Meta<ButtonProps>
 
