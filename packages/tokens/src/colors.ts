a84e4e0f3662577b1f28831c9d58c63e2843660b
rsilva33 <ronaldo.silva.jr0105@gmail.com>
2023-11-22 14:36:31 +0000

    add new test color to tokens

--- packages/tokens/src/colors.ts
+++ packages/tokens/src/colors.ts
@@ -5,8 +5,7 @@ export const colors = {
   gray100: '#E1E1E6',
   gray200: '#A9A9B2',
   gray400: '#7C7C8A',
-  gray500: '#505059',
-  gray600: '#323238',
+  gray500: '#505059', 
   gray700: '#29292E',
   gray800: '#202024',
   gray900: '#121214',
@@ -15,4 +14,6 @@ export const colors = {
   ignite500: '#00875F',
   ignite700: '#015F43',
   ignite900: '#00291D',
+
+  test: '#fff',
 }
